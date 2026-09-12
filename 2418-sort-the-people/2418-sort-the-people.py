24cc933ea4b8ba4d08011d942a38549d844ccae8
K Sai Krishna <154864664+saikrishna1235@users.noreply.github.com>
2026-09-12 12:23:10 +0530

    Time: 3 ms (92.46%), Space: 12.5 MB (89.29%) - LeetHub

---
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -0,0 +1,7 @@
+class Solution:
+    def sortPeople(self, names, heights):
+        people = list(zip(heights, names))
+        
+        people.sort(reverse=True)
+        
+        return [name for height, name in people]
